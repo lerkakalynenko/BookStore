55738562bc986eb585acf9ba382b32e175b646c8
Valeriya <valeria170503@gmail.com>
2022-02-17 22:46:56 +0200

    authorization

--- BookStore.Domain.Core/ApplicationContext.cs
+++ BookStore.Domain.Core/ApplicationContext.cs
@@ -4,17 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BookStore.Domain.Core.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookStore.Infrastructure.Data
 {
-    public class ApplicationContext : DbContext
+    public class ApplicationContext : IdentityDbContext<IdentityUser>
     {
         public DbSet<Category> Categories { get; set; }
         public DbSet<Book> Books { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<User> Users { get; set; }
+        public ApplicationContext()
+        {
 
+        }
         public ApplicationContext(DbContextOptions<ApplicationContext> options)
             : base(options)
         {
